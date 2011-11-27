d669c250ab3cb4cdfb999633bc861b694a2fc87d
Dmitry Sobolev <propp.ds@gmail.com>
2011-11-27 10:18:44 +0600

    Переписал ссылки на главной странице админки

--- protected/modules/admin/views/default/index.php
+++ protected/modules/admin/views/default/index.php
@@ -6,10 +6,10 @@ $this->breadcrumbs = array(
 <h1>Страница администратора</h1>
 
 <ul class="menu-map">
-    <li><a href="<?php echo Yii::app()->urlManager->createUrl('article/add'); ?>">Добавить статью</a> - добавление новой статьи</li>
-    <li><a href="<?php echo Yii::app()->urlManager->createUrl('article/manage'); ?>">Найти статью</a> - поиск статей и возможность применения различных операций (удаление, редактироание, просмотр) к ним</li>
-    <li><a href="<?php echo Yii::app()->urlManager->createUrl('tags/add'); ?>">Добавить тег</a> - добавление новго тега</li>
-    <li><a href="<?php echo Yii::app()->urlManager->createUrl('tags/manage'); ?>">Найти теги</a> - поиск и оперирование тегами</li>
-    <li><a href="<?php echo Yii::app()->urlManager->createUrl('studio/add'); ?>">Добавить студию</a> - добавление новой студии в список</li>
-    <li><a href="<?php echo Yii::app()->urlManager->createUrl('studio/manage'); ?>">Найти студию</a> - поиск и оперирование студиями</li>
+    <li><a href="<?php echo Yii::app()->urlManager->createUrl('admin/article/create'); ?>">Добавить статью</a> - добавление новой статьи</li>
+    <li><a href="<?php echo Yii::app()->urlManager->createUrl('admin/article/admin'); ?>">Найти статью</a> - поиск статей и возможность применения различных операций (удаление, редактироание, просмотр) к ним</li>
+    <li><a href="<?php echo Yii::app()->urlManager->createUrl('admin/tags/create'); ?>">Добавить тег</a> - добавление новго тега</li>
+    <li><a href="<?php echo Yii::app()->urlManager->createUrl('admin/tags/admin'); ?>">Найти теги</a> - поиск и оперирование тегами</li>
+    <li><a href="<?php echo Yii::app()->urlManager->createUrl('admin/studio/create'); ?>">Добавить студию</a> - добавление новой студии в список</li>
+    <li><a href="<?php echo Yii::app()->urlManager->createUrl('admin/studio/admin'); ?>">Найти студию</a> - поиск и оперирование студиями</li>
 </ul>
