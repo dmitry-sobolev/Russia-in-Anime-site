1595f47dd12049871fe5addab815329b7cf329ad
Dmitry Sobolev <propp.ds@gmail.com>
2011-11-30 22:18:57 +0600

    Задал язык сайта

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,6 +7,7 @@
 return array(
     'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
     'name' => 'Russia in Anime',
+    'language' => 'ru',
     // preloading 'log' component
     'preload' => array('log'),
     // autoloading model and component classes
