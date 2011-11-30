a4b41ed72a165361d9532ad4c2c19e29ae05f466
Dmitry Sobolev <propp.ds@gmail.com>
2011-11-30 22:19:39 +0600

    Переделал миграцию. Теперь она рабочая.

--- protected/migrations/m111129_153945_alter_table_articles_add_scriptWriter_add_originalRole_change_format.php
+++ protected/migrations/m111129_153945_alter_table_articles_add_scriptWriter_add_originalRole_change_format.php
@@ -15,13 +15,17 @@ class m111129_153945_alter_table_articles_add_scriptWriter_add_originalRole_chan
 	// Use safeUp/safeDown to do migration with transaction
 	public function safeUp()
 	{
-            $this->alterColumn('article', 'format', 'int foreign key tbl_formats(FtID) on delete set null on update cascade');
+            $this->alterColumn('article', 'format', 'int');
             $this->addColumn('article', 'scriptWriter', 'varchar(100)');
-            $this->addColumn('article', 'originalRole', 'int foreign key tbl_original_roles(ORID) on delete set null on update cascade');
+            $this->addColumn('article', 'originalRole', 'int');
+            $this->addForeignKey('format4article', 'article', 'format', 'tbl_formats', 'FtID', 'set null', 'cascade');
+            $this->addForeignKey('original_role4article', 'article', 'originalRole', 'tbl_original_roles', 'ORID', 'set null', 'cascade');
 	}
 
 	public function safeDown()
 	{
+            $this->dropForeignKey('original_role4article', 'article');
+            $this->dropForeignKey('format4article', 'article');
             $this->dropColumn('article', 'scriptWriter');
             $this->dropColumn('article', 'originalRole');
             $this->alterColumn('article', 'format', "enum('tv', 'ova', 'movie')");
